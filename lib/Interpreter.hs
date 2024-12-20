e89963d6c5d471a630a54c7ef45a7480edf8bbcc
Raju Ahmed <raju.ahmed@optimizely.com>
2024-12-21 03:45:31 +0600

    implemented expression evaluation

---
+++ lib/Interpreter.hs
@@ -0,0 +1,79 @@
+module Interpreter where
+import Ast
+import Token
+
+data LoxValue = LoxNil
+              | LoxBool Bool
+              | LoxNumber Double
+              | LoxString String
+              deriving (Show, Eq)
+
+data RuntimeError = RuntimeError (Maybe TokenWithContext) String
+  deriving (Show, Eq)
+
+
+isTruthy :: LoxValue -> Bool
+isTruthy LoxNil = False
+isTruthy (LoxBool False) = False
+isTruthy _ = True
+
+operandNotNumber :: TokenWithContext -> RuntimeError
+operandNotNumber token = RuntimeError (Just token) "Operand must be a number."
+
+bothOperandsNotNumber :: TokenWithContext -> RuntimeError
+bothOperandsNotNumber token = RuntimeError (Just token) "Operands must be numbers."
+
+unaryOp:: TokenWithContext -> LoxValue -> Either RuntimeError LoxValue
+unaryOp (TokenWithContext Minus _ _) (LoxNumber n) = Right $ LoxNumber (-n)
+unaryOp token@(TokenWithContext Minus _ _) _ = Left $ operandNotNumber token
+unaryOp (TokenWithContext Bang _ _) value =  Right $ LoxBool (not (isTruthy value))
+unaryOp _ _ = Left $ RuntimeError Nothing "Failed to evaluate unary operation"
+
+binaryOp :: TokenWithContext -> LoxValue -> LoxValue -> Either RuntimeError LoxValue
+binaryOp (TokenWithContext Plus _ _) (LoxNumber l) (LoxNumber r) = Right $ LoxNumber (l + r)
+binaryOp (TokenWithContext Plus _ _) (LoxString l) (LoxString r) = Right $ LoxString (l ++ r)
+binaryOp token@(TokenWithContext Plus _ _) _ _ = Left $ RuntimeError (Just token) "Operands must be two numbers or two strings."
+
+binaryOp (TokenWithContext Minus _ _) (LoxNumber l) (LoxNumber r) = Right $ LoxNumber (l - r)
+binaryOp token@(TokenWithContext Minus _ _) _ _ = Left $ bothOperandsNotNumber token
+
+binaryOp (TokenWithContext Slash _ _) (LoxNumber l) (LoxNumber r) = Right $ LoxNumber (l / r)
+binaryOp token@(TokenWithContext Slash _ _) _ _ = Left $ bothOperandsNotNumber token
+
+binaryOp (TokenWithContext Star _ _) (LoxNumber l) (LoxNumber r) = Right $ LoxNumber (l * r)
+binaryOp token@(TokenWithContext Star _ _) _ _ = Left $ bothOperandsNotNumber token
+
+binaryOp (TokenWithContext Greater _ _) (LoxNumber l) (LoxNumber r) = Right $ LoxBool (l > r)
+binaryOp token@(TokenWithContext Greater _ _) _ _ = Left $ bothOperandsNotNumber token
+
+binaryOp (TokenWithContext GreaterEqual _ _) (LoxNumber l) (LoxNumber r) = Right $ LoxBool (l >= r)
+binaryOp token@(TokenWithContext GreaterEqual _ _) _ _ = Left $ bothOperandsNotNumber token
+
+binaryOp (TokenWithContext Less _ _) (LoxNumber l) (LoxNumber r) = Right $ LoxBool (l < r)
+binaryOp token@(TokenWithContext Less _ _) _ _ = Left $ bothOperandsNotNumber token
+
+binaryOp (TokenWithContext LessEqual _ _) (LoxNumber l) (LoxNumber r) = Right $ LoxBool (l <= r)
+binaryOp token@(TokenWithContext LessEqual _ _) _ _ = Left $ bothOperandsNotNumber token
+
+-- FixMe: Haskell value comparison is not the same as Lox value comparison
+binaryOp (TokenWithContext EqualEqual _ _) l r = Right $ LoxBool (l == r)
+binaryOp (TokenWithContext BangEqual _ _) l r = Right $ LoxBool (l /= r)
+
+binaryOp _ _ _ = Left $ RuntimeError Nothing "Failed to evaluate binary operation"
+
+evalExpression :: Expression -> Either RuntimeError LoxValue
+evalExpression (Literal (TokenWithContext (NumberToken n) _ _)) = Right $ LoxNumber n
+evalExpression (Literal (TokenWithContext (StringToken s) _ _)) = Right $ LoxString s
+evalExpression (Literal (TokenWithContext TrueToken _ _)) = Right $ LoxBool True
+evalExpression (Literal (TokenWithContext FalseToken _ _)) = Right $ LoxBool False
+evalExpression (Literal (TokenWithContext Nil _ _)) = Right $ LoxNil
+
+evalExpression (Unary token expr) = evalExpression expr >>= unaryOp token
+
+evalExpression (Binary left token right) = do
+  leftValue <- evalExpression left
+  rightValue <- evalExpression right
+  binaryOp token leftValue rightValue
+
+evalExpression (Grouping expr) = evalExpression expr
+evalExpression _ = Left $ RuntimeError Nothing "Failed to evaluate expression"
---
+++ spec/InterpreterSpec.hs
@@ -0,0 +1,28 @@
+module InterpreterSpec where
+
+import SpecHelper
+import Token
+import Lexer
+import Ast
+import Parser
+import Interpreter
+import Data.Either 
+
+scanResult :: String -> [TokenWithContext]
+scanResult = fromRight [] . scan
+
+scanAndParse :: String -> Expression
+scanAndParse = fromRight (Literal (TokenWithContext (NumberToken 0) 0 0)) . fst . parseExpression . scanResult
+
+spec :: Spec
+spec = describe "interpreter" $ do
+  it "interprets expressions" $ do
+    evalExpression (scanAndParse "1") `shouldBe` Right (LoxNumber 1)
+    evalExpression (scanAndParse "1 + 2") `shouldBe` Right (LoxNumber 3)
+    evalExpression (scanAndParse "1 + 2 / 2") `shouldBe` Right (LoxNumber 2)
+    evalExpression (scanAndParse "1 + 2 * 2") `shouldBe` Right (LoxNumber 5)
+    evalExpression (scanAndParse "\"fo\" + \"o\"") `shouldBe` Right (LoxString "foo")
+    evalExpression (scanAndParse "1 + true") `shouldBe` Left (RuntimeError (Just (TokenWithContext (Plus) 1 3)) "Operands must be two numbers or two strings.")
+    evalExpression (scanAndParse "!true") `shouldBe` Right (LoxBool False)
+    evalExpression (scanAndParse "1 == 1") `shouldBe` Right (LoxBool True)
+    evalExpression (scanAndParse "1 > 2") `shouldBe` Right (LoxBool False)    
